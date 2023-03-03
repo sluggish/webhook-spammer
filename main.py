dd8ec7091f04505e8fab071a3df7a34f0863fa88
sluggish <riq@riseup.net>
2023-03-02 19:46:57 -0700

    new file:   main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import discord
+from discord import Webhook
+
+import aiohttp
+import colorama
+import asyncio
+
+from colorama import init
+
+init()
+
+async def send_webhook_msg(webhook, amount: int, username, message):
+        async with aiohttp.ClientSession() as session:
+            web = Webhook.from_url(webhook, session=session)
+            for msg in range(amount + 1):
+                 await web.send(message, username=username)
+                 print("Sent {} messages".format(msg))
+
+
+async def main():
+    webhook = input("Webhook URL to spam: ")
+    amount = int(input("Amount of messages to send: "))
+    username = input("Username to send webhook as: ")
+    message = input("Message to send to webhook: ")
+
+    await send_webhook_msg(webhook, amount, username, message)
+
+asyncio.run(main())
